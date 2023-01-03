ee770049375ba4bdb209dd0402977879d053b411
wlin00 <616294069@qq.com>
2023-01-03 18:04:50 +0800

    fix: 登录接口确保验证码未使用过

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::SessionsController < ApplicationController
     end
     # 若当前非测试环境，需要校验本次会话创建前是否发送验证码
     if !Rails.env.test?
-      canSignInFlag = ValidationCode.exists? email: params[:email], code: params[:code]
+      canSignInFlag = ValidationCode.exists? email: params[:email], code: params[:code], used_at: nil # 确保数据库中有一条未使用过的验证码
       return render status: :unauthorized, json: { message: '用户名或验证码错误' } unless canSignInFlag
     end
     # 创建会话，若当前user是否存在于User表中，则查询到当user，否则创建新的user（使用rails的 User.find_or_create_by 方法来创建或查询）
